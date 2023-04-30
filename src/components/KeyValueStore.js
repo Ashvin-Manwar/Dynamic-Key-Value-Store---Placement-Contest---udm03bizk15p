bc48feb3278483f17ef799718adff287d8e93bed
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-04-30 16:07:45 +0530

    Update KeyValueStore.js

--- src/components/KeyValueStore.js
+++ src/components/KeyValueStore.js
@@ -5,35 +5,66 @@ const KeyValueStore = () => {
   
     const [keyValues, setKeyValues] = useState({});
   
-    const updateKeyValue = (key, value) => {};
+    const updateKeyValue = (key, value) => {
+      setKeyValues(prevState=>{
+        return {...prevState,[key]:value};
+      })
+    };
   
-    const handleUpdateClick = () => {};
+    const handleUpdateClick = () => {
+      const queryParams=Object.keys(keyValues)
+      .map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(keyValues[key])}`)
+      .join('&');
+
+      window.history.replaceState(null,null,`?${queryParams}`)
+    };
   
-    const handleDeleteClick = (key) => {};
+    const handleDeleteClick = (key) => {
+      setKeyValues(prevState=>{
+        const newState={...prevState};
+        delete newState[key];
+        return newState;
+      })
+    };
   
-    useEffect(() => {}, []);
+    useEffect(() => {
+      const queryParams=new URLSearchParams(window.location.search);
+      const newKeyValue={};
+      for(let [key,value] of queryParams.entries()){
+        newKeyValue[decodeURIComponent(key)]=decodeURIComponent(value);
+      }
+      setKeyValues(newKeyValue);
+    }, []);
   
     return (
       <div>
         <h1>Key Value Store</h1>
           <div>
             {/* Map the key-value using the below HTML */}
+            {Object.entries(keyValues).map(([key,value])=>(
               <div key={key} className='key-value-div'>
-                <span className='key-field'>key:</span>
+                <span className='key-field'>{key}:</span>
                 <input
                     className='value-field'
                     type="text"
+                    value={value}
+                    onChange={(e)=>updateKeyValue(key,e.target.value)}
                 />
-                <button className='delete-btn'>Delete</button>
+                <button className='delete-btn'
+                onClick={()=>handleDeleteClick(key)}
+                >Delete</button>
               </div>
+            ))}
 
             {/* Do not include Update button, while mapping the key-value */}
-            <button className='update-btn'>Update Values</button>
+            <button className='update-btn'
+            onClick={handleUpdateClick}
+            >Update Values</button>
           </div>
-
-          <p>No key values found in URL.</p>
+            {Object.keys(keyValues).length===0 &&
+          <p>No key values found in URL.</p>}
       </div>
-    )
-}
+    );
+};
 
 export default KeyValueStore;
